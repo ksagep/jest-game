6891ca8843d2c1a9b6206b63c86a896604270c77
ksagep <sandor.kisgyorgy@gmail.com>
2022-06-27 18:37:04 +0000

    Add plus red possibilities to zje page.

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,24 +2,31 @@
  * @jest-environment jsdom
  */
 
-const { exportAllDeclaration, toBindingIdentifierName } = require("@babel/types");
-const { default: test } = require("node:test");
-const { describe } = require("yargs");
-const { game } = require("../game");
+ const { game } = require("../game");
 
-beforeAll (() => {
-    let fs = required("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
-});
 
-describe("game object contains correct keys", () => {
-    test("score key exists", () => {
-        expect("score" in game).toBe(true);
-    });
-    test("currentGame key exists", () => {
-        expect("currentGame" in game).toBe(true);
-    });
-});
+ beforeAll(() => {
+     let fs = require("fs");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+     document.close();
+ });
+ 
+ describe("game object contains correct keys", () => {
+     test("score key exists", () => {
+         expect("score" in game).toBe(true);
+     });
+     test("currentGame key exists", () => {
+         expect("currentGame" in game).toBe(true);
+     });
+     test("playerMoves key exists", () => {
+         expect("playerMoves" in game).toBe(true);
+     });
+     test("choices key exists", () => {
+         expect("choices" in game).toBe(true);
+     });
+     test("choices contain correct ids", () => {
+         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+     });
+ });
